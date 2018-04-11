36de6c1f2f789926fd4ceb6f1e8141e8d9cff9dd
qianliao.zhuang <qianliao.zhuang@weimob.com>
2018-04-11 09:53:17 +0800

    jackson

--- src/test/java/com/example/utils/JacsonDuotaiTest.java
+++ src/test/java/com/example/utils/JacsonDuotaiTest.java
@@ -3,7 +3,11 @@ package com.example.utils;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.Annotated;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import lombok.Data;
+
+import java.lang.annotation.Annotation;
 
 /**
  * @author qianliao.zhuang
@@ -53,16 +57,47 @@ public class JacsonDuotaiTest {
         objectMapper.registerSubtypes(Person.class);
         objectMapper.registerSubtypes(new NamedType(Dog.class,"dog"));
         objectMapper.registerSubtypes(new NamedType(Cat.class,"cat"));*/
-        String personJson = objectMapper.writeValueAsString(person);
-        System.out.println(personJson);
-
-        Person person1 = objectMapper.readValue(personJson, new TypeReference<Person>() {});
-        System.out.println();
 
         String testJson = objectMapper.writeValueAsString(test);
-        System.out.println(testJson);
+        System.out.println("test: " + testJson);
 
         Test test1 = objectMapper.readValue(testJson, new TypeReference<Test>() {});
         System.out.println();
+
+        ObjectMapper objectMapper1 = new ObjectMapper().setAnnotationIntrospector(new DisablingJsonTypeInfoIntrospector());
+        String hideJsonTypeInfo = objectMapper1.writeValueAsString(test);
+        System.out.println("hideJsonTypeInfo: " + hideJsonTypeInfo);
+    }
+
+    public static class DisablingJsonTypeInfoIntrospector extends JacksonAnnotationIntrospector {
+        /**
+         * 网上说这个方法可以隐藏注解，但是貌似行不通
+         * 可能是可以隐藏自定义注解，且自定义注解上有 @JacksonAnnotationsInside 标识
+         * @param ann
+         * @return
+         */
+        @Override
+        public boolean isAnnotationBundle(Annotation ann) {
+            if (ann.annotationType().equals(JsonTypeInfo.class)) {
+                return false;
+            } else {
+                return super.isAnnotationBundle(ann);
+            }
+        }
+
+        /**
+         * 隐藏 JsonTypeInfo 注解
+         * @param annotated
+         * @param annoClass
+         * @param <A>
+         * @return
+         */
+        @Override
+        protected <A extends Annotation> A _findAnnotation(Annotated annotated, Class<A> annoClass) {
+            if (annoClass.equals(JsonTypeInfo.class)) {
+                return null;
+            }
+            return super._findAnnotation(annotated, annoClass);
+        }
     }
 }
